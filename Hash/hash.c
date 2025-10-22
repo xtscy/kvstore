ab5105f4fd942ce456f808d9d8142bb476ad2ef3
xst <690311995@qq.com>
2025-10-22 11:02:44 +0000

    双重哈希

--- Hash/hash.c
+++ Hash/hash.c
@@ -1,66 +1,198 @@
-#include "hash.h"
+# include "hash.h"
 
-double_hash_table_t *hash_table = NULL;
 
-fixed_size_pool_t *hash_pool = NULL;
-fixed_size_pool_t *handle_pool = NULL;
-// 在入口函数初始化handle_pool
+// 这里接收外部传入的hash_table
 
-// 然后直接在hash_create中直接向handle_pool申请
-double_hash_table_t *hash_table = NULL;
+// hash_table_t global_hash_table = {0};
+hash_table_t *global_hash_table = {0};
 
-double_hash_table_t *hash_table_create(size_t initial_size) {
-    // 先向内存池申请存储table_t表结构，然后再申请hash_entry_t存储kv数据，这里连续向内存池申请，具有缓存连续性
-    // initial_size是kv_data数组的个数
-    // 从句柄池中申请块来存放table_t
-    hash_table = fixed_pool_alloc(handle_pool);
-    // 然后初始化hash_table
-    //! 这里应该也不用加锁，后续再看，先加锁
-    pthread_rwlock_wrlock(&hash_table->rwlock);
-    hash_table->table_size = initial_size;
-    hash_table->cur_data_size = 0;
-    hash_table->dele_count = 0;
-    //在锁里面申请内存
-    pthread_rwlock_unlock(&hash_table->rwlock);
-    // 这里块大小和块个数
-    // 块个数就是initial_size
+
+hash_table_t* hash_table_create(size_t size, hash_table_t *table) {
+
+    //这里先直接malloc开辟，后续可以用一个内存池
+    // 向内存池中申请，在一个内存池中管理多种数据结构
+    // 这里init一般直接malloc都差不多，因为只会调用一次
+
+    //这里可以判断一下size是否是质数    
+    hash_table_t *new_table = malloc(sizeof(hash_table_t));
     
-    fixed_pool_create()
+    new_table->table = malloc(sizeof(hash_item_t) * size);
+    if (new_table->table == NULL) {
+        printf("malloc hash item array failed\n");
+        exit(-3);
+    }
+
+    memset(new_table, 0, sizeof(hash_item_t) * size);
+
+
+    new_table->size = size;
     
-
-}
- 
-void hash_table_destroy(double_hash_table_t *table) {
-
+    return new_table;
 }
 
-_Bool hash_table_put(double_hash_table_t *table, const char *key, const void *value, int value_size) {
+// 这里实现对外的字符串获取整形key
 
+int abs_val(int val) {
+    return val < 0 ? -val : val;
+}
+
+// 双重哈希
+// 第一个哈希，直接取模得到位置
+// 第二个哈希，计算遍历的步长
+
+unsigned long hash_init_pos(unsigned long key, hash_table_t *table) {
+    return key % table->size;
+}
+
+unsigned long hash_step_size(unsigned long key, hash_table_t *table) {
+    // 获得相对于size互质的步长
+    // 互质就能遍历到每一个位置,不会提前回到起点
+    // 当然这里table的size一定是质数才行
+    // 这里拿范围[1, size - 1], 因为size是质数，所以每个数都必然互质，那么每个数都可以是步长
+    return 1 + (key % (table->size - 1));
+}
+
+unsigned long string_to_key(const char *input) {
+
+    unsigned long hash = 5381;
+    int c;
+    
+    while ((c = *input++)) {
+        hash = ((hash << 5) + hash) + c; // hash * 33 + c
+    }
+    return hash;
+}
+
+void destroy_table(hash_table_t *table) {
+
+    if (table) {
+        free(table->table);
+        free(table);
+    }
+    
+}
+// 这里拿到当前位置的key, 判断当前table位置的key
+// 这里key是数组下标，并不是实际存储的void*的key数据
+// 这里传数组下标，还需要把新的key传过来
+// 这里判断key_pos位置是否key_data和当前void*的二进制位相同
+// 在判断之前
+bool key_compare(hash_item_t *item, void *key_data, uint16_t key_size) {
+
+    // 这里首先长度必须相同，然后就是每个字节都相同
+    // 那么就认定两个key是相同的
+    if (item->key_size != key_size) return false;
+
+    if(!memcmp(item->key, key_data, key_size)) {
+        return true;
+    }
+    
+    return false;
 }
 
 
-void *hash_table_get(double_hash_table_t *table, const char *key) {
+// 这里实现多套API针对不同的类型
+// 由于key永远都是存储字符串
+// 所以下面的insert可以直接复用，但是拿的时候就不知道存的是什么类型，所以
+// 还是需要增添一个标识类型的枚举字段
+// 这里用户外部调用函数，把非整形类型转换成整形类型
+bool insert(unsigned long key_pos, void *key_data, uint16_t key_size,
+    void* val, uint16_t val_size, hash_table_t *table, value_type_t type) {
 
+    if (key_data == NULL || val == NULL || table == NULL) {
+        printf("insert parameter NULL\n");
+        exit(-3);
+    }
+    // 先通过hash1获取初始位置
+    int init_pos = hash_init_pos(key_pos, table);
+    int step = hash_step_size(key_pos, table);
+    int cur_pos = 0;
+    hash_item_t *cur_item = {0};
+    for (int i = 0; i < table->size; i++) {
+        cur_pos = (init_pos + step * i) % table->size;
+        //* 这里判断，key_size相同并且key数据相同那么才会认定为同一个
+        //! 这里判断当前位置是否被删除或者是被
+        cur_item = &table->table[cur_pos];
+        if (!cur_item->is_occupied || cur_item->is_deleted) {
+            if (cur_item->is_deleted) {
+                free(cur_item->key);
+                free(cur_item->val);
+            }
+            // 直接使用当前位置
+            cur_item->is_occupied = true;
+            cur_item->key = key_data;
+            cur_item->key_size = key_size;
+            cur_item->val = val;
+            cur_item->val_size = val_size;
+            cur_item->is_deleted = false;
+            cur_item->val_type = type;
+            return true;
+        }
+        
+        if (key_compare(&table->table[cur_pos], key_data, key_size)
+        && cur_item->val_type == type) {
+            // 如果判断成功，即下标位置的key相同，直接更改value值
+            // 那么这里还需要回收之前的值吗
+            //! 当前是直接malloc开辟的值,那么直接free即可
+            //! 后续有数据内存池，就调用外部free接口
+            free(cur_item->val);
+            table->table[cur_pos].val = val;
+            table->table[cur_pos].val_size = val_size;
+            return true;
+        }
+        
+        // 继续循环
+    }
+    
+    // 没有能够插入数据的位置,这里要实现一个自动扩容机制，当负载因子达到某个阈值时，进行扩容
+    // 这里先就这样，如果没有位置插入，那么直接返回false;
+    return false;
 }
 
-bool hash_table_remove(double_hash_table_t *table, const char *key) {
 
+long hash_table_search_int(char *key, uint16_t key_size, hash_table_t *table) {
+
+
+    
 }
 
-bool hash_table_contains(double_hash_table_t *table, const char *key) {
+double hash_table_search_double(char *key, uint16_t key_size, hash_table_t *table) {
 
+
+    
 }
 
-size_t hash1(const char *key, size_t table_size) {
+char* hash_table_search_string(char *key, uint16_t key_size, uint16_t *target_length, hash_table_t *table) {
 
+        
+    
 }
 
-size_t hash2(const char *key, size_t table_size) {
 
+void* hash_table_search(unsigned long key, void *key_data, uint16_t key_size, hash_table_t *table) {
+    unsigned long init_pos = 0, step_size = 0, cur_pos = 0;
+    init_pos = hash_init_pos(key, table);
+    step_size = hash_step_size(key, table);
+    for (int i = 0; i < table->size; i++) {
+        // 判断当前位置
+        cur_pos = (init_pos + step_size * i) % table->size;
+        // 没被占用或者被删除,说明当前位置是空
+        if (!table->table[cur_pos].is_occupied || table->table[cur_pos].is_deleted) {
+            break;
+        }
+        // 当前位置为有效值，判断是否和搜索键相同
+
+        if (key_compare(&table[cur_pos], key_data, key_size)) {
+            return table->table[cur_pos].val;
+        }
+        
+    }
+    
 }
 
 
-void hash_table_cleanup(double_hash_table_t *table) {
 
-}
+
+
+
+
 
--- Hash/hash.h
+++ Hash/hash.h
@@ -1,46 +1,53 @@
+
 #define _GNU_SOURCE
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
 #include <stdint.h>
-#include <stddef.h>
-#include "../memory_pool/memory_pool.h"
 #include <stdbool.h>
-#include <pthread.h>
-#include "../kv_task.h"
+#include <string.h>
 
 
-// 这里用指针指向key起始位置，key从数据池中分配
-// 这样就能分配任意大小了
-// 这个数据池实现，是没有块封装直接操作字节
-typedef struct hash_entry_s {
-    size_t key_size;
-    size_t value_size;
-    value_type_t value_type;
-    char kv_data[];
-} hash_entry_t;
+typedef enum value_type_e {
+    Unknow,
+    Int,
+    Double,
+    String,
+} value_type_t;
 
 
-typedef struct double_hash_table_s {
-    //这里使用读写锁
-    pthread_rwlock_t rwlock;
-    hash_entry_t *table;
-    size_t table_size;
-    size_t cur_data_size;
-    size_t dele_count;
-} double_hash_table_t;
+typedef struct hash_item_s {
 
+    char *key;
+    void *val;
+    uint16_t key_size;// 不计算前导和后导0
+    uint16_t val_size;
+    bool is_occupied;
+    bool is_deleted;
+    value_type_t val_type;
 
-extern double_hash_table_t *hash_table_create(size_t initial_size);
-extern void hash_table_destroy(double_hash_table_t *table);
+} hash_item_t;
 
-// 这里在外部已经判断了值的类型，所以这里可以直接传值大小，不用再去判断值类型了
-extern _Bool hash_table_put(double_hash_table_t *table, const char *key, const void *value, int value_size);
+typedef struct hash_table_s {
+    hash_item_t *table;
+    size_t size;
 
-extern void* hash_table_get(double_hash_table_t *table, const char *key);
-extern bool hash_table_remove(double_hash_table_t *table, const char *key);
+} hash_table_t;
 
-extern bool hash_table_contains(double_hash_table_t *table, const char *key);
+extern hash_table_t* hash_table_create(size_t, hash_table_t*);
+extern void destroy_table(hash_table_t*);
 
-size_t hash1(const char *key, size_t table_size);
-size_t hash2(const char *key, size_t table_size);
+extern unsigned long hash_init_pos(unsigned long, hash_table_t*);
+extern unsigned long hash_step_size(unsigned long, hash_table_t*);
+extern bool insert(unsigned long, void*, uint16_t, void*, uint16_t, hash_table_t*, value_type_t);
 
+extern unsigned long string_to_key(const char*);
+extern bool key_compare(hash_item_t*, void*, uint16_t);
 
-void hash_table_cleanup(double_hash_table_t *table);
+extern void* hash_table_search(unsigned long, void*, uint16_t, hash_table_t*);
+
+extern long hash_table_search_int(char*, uint16_t, hash_table_t*);
+extern double hash_table_search_double(char*, uint16_t, hash_table_t*);
+extern char* hash_table_search_string(char*,uint16_t, uint16_t*, hash_table_t*);
+
